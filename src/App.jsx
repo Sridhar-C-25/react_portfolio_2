6e467fc2139c58469a6a3769b3c3c71d1b1be293
Sohaib Manah <souhaibemanah@gmail.com>
2023-02-18 04:47:21 +0100

    update the favicon

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Layouts/Navbar";
 import Skills from "./components/Skills";
 import Service from "./components/Services";
 import Projects from "./components/Projects";
-import Testimonials from "./components/Testimonials";
+// import Testimonials from "./components/Testimonials";
 import Aboutme from "./components/Aboutme";
 import Contact from "./components/Contact";
 import { useEffect, useState } from "react";
