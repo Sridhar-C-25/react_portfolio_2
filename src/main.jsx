9764c70256c043d3b1d212fe0a0590fc3b2418b7
unknown <sverbraeken60@gmail.com>
2023-04-19 19:29:27 -0600

    Added HEAD with react-helmet module

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import Head from './head'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Head />
     <App />
   </React.StrictMode>
 )
