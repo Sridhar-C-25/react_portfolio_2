839ddcf4e8b1977e254e73170c9d8c28924f4495
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-14 19:19:08 -0700

    seven

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,24 +1,45 @@
-import { createElement, useState } from "react";
-import {content} from "../content";
-
-const customStyles = {
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      marginRight: "-50%",
-      transform: "translate(-50%, -50%)",
-      maxWidth: "23rem",
-      width: "90%",
-    },
-    overlay: {
-      padding: "2rem",
-    },
-};
+import { content } from "../Content";
 
 const Skills = () => {
-    const { skills } = content;
-}
+  const { skills } = content;
 
-export default Skills
+  return (
+    <section className="min-h-fit bg-bg_light_primary" id="skills">
+      <div className="md:container px-5 py-14">
+        <h2 className="title" data-aos="fade-down">
+          {skills.title}
+        </h2>
+        <h4 className="subtitle" data-aos="fade-down">
+          {skills.subtitle}
+        </h4>
+        <br />
+        <div className="flex flex-wrap gap-4 justify-center">
+          {skills.skills_content.map((skill, i) => (
+            <div
+              key={i}
+              data-aos="fade-up"
+              data-aos-delay={i * 400}
+              className="bg-white sm:cursor-default
+               relative group w-full flex items-center
+                gap-5 p-5 max-w-sm rounded-md border-2 border-slate-200"
+            >
+              <div>
+                <img
+                  src={skill.logo}
+                  alt="..."
+                  className="w-10 group-hover:scale-125 duration-200"
+                />
+              </div>
+              <div>
+                <h6>{skill.name}</h6>
+                <p className="italic">{skill.para}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
