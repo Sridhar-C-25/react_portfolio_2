11bee77688502b81ebfd717ccca4e8c4320c9444
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-14 18:27:00 -0700

    another painful test of skills removal

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 // import components
 import Hero from "./components/Hero";
 import Navbar from "./Layouts/Navbar";
-import Skills from "./components/Skills";
 import Service from "./components/Services";
 import Projects from "./components/Projects";
 import Testimonials from "./components/Testimonials";
