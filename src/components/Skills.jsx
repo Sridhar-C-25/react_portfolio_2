a93644de03232a513354c3c6710450cf7744731b
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-14 18:47:31 -0700

    five

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,3 +1,24 @@
+import { createElement, useState } from "react";
 import {content} from "../Content";
+
+const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      maxWidth: "23rem",
+      width: "90%",
+    },
+    overlay: {
+      padding: "2rem",
+    },
+};
+
+const Skills = () => {
+    const { skills } = content;
+}
 
 export default Skills
