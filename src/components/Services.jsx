c1e9f4feb2a03530325327567123d00534977918
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-20 13:44:04 -0700

    revising services and content

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -36,7 +36,8 @@ const Services = () => {
               key={i}
               data-aos="fade-up"
               data-aos-delay={i * 600}
-              className="min-w-[14rem] duration-300 cursor-pointer border-2 border-slate-200 rounded-xl text-center bg-bg_light_primary p-6 flex-1 group-hover:blur-sm hover:!blur-none"
+              className="min-w-[14rem] duration-300 cursor-pointer border-2 border-slate-200 rounded-xl text-center bg-bg_light_primary p-6 flex-1 group-hover:blur-none hover:!cursor-default"
+              // Remove the "blur-sm" class on hover and change cursor behavior
             >
               {getLogo(content.logo)}
               <h6 className="my-3">{content.title}</h6>
