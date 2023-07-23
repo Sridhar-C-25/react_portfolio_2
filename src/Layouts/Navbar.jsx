0bd190716a2c1a819d9cd3e60040de2e465af831
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-22 23:51:56 -0700

    navigation update plus removal of testimonials

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -18,14 +18,14 @@ const Navbar = () => {
       </div>
       {showMenu && (
         <nav
-          className={`fixed top-20 left-10 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-md text-dark_primary duration-300 radius-30%`}
+          className={`fixed top-20 left-1/2 transform -translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300`}
         >
           {nav.map((item, i) => (
             <a
               key={i}
               href={item.link}
               onClick={() => setActive(i)}
-              className={`text-xl p-2.5 rounded-full sm:cursor-pointer
+              className={`text-xl p-2.5 rounded-full sm:cursor-pointer bg-cylinder
      ${i === active && "bg-dark_primary text-white"} `}
             >
               {createElement(item.icon)}
