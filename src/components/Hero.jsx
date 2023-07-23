44b4abde3add71089fb3465afcb7c9f8be9b239a
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-22 17:20:49 -0700

    button removal from the top

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,9 +22,6 @@ const Hero = () => {
         <div className="pb-16 px-6 pt-5" data-aos="fade-down">
           <h2>{hero.title}</h2>
           <br />
-          <div className="flex justify-end">
-            <button className="btn">{hero.btnText}</button>
-          </div>
           <div className="flex flex-col gap-10 mt-10">
             {hero.hero_content.map((content, i) => (
               <div
