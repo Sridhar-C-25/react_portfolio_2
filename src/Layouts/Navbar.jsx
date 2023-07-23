72a58611d284b37e2cddb7e59dfd03a6d86a9e4d
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-23 15:54:43 -0700

    third update

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -11,28 +11,32 @@ const Navbar = () => {
   return (
     <div className="w-full flex justify-center relative">
       <div
-        className="sm:cursor-pointer fixed top-10 left-10 z-[999] rounded-lg bg-white/40 p-2"
+        className="sm:cursor-pointer z-[999] rounded-lg bg-white/40 p-2"
         onClick={() => setShowMenu(!showMenu)}
       >
         <HiMenuAlt2 size={34} />
-      </div>
-      {showMenu && (
         <nav
-          className={`fixed top-14 left-2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300`}
+          className={`absolute top-full right-1/2 transform translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-md text-dark_primary duration-300 ${
+            showMenu ? "opacity-100 translate-y-0" : "opacity-0 translate-y-3"
+          }`}
         >
           {nav.map((item, i) => (
             <a
               key={i}
               href={item.link}
-              onClick={() => setActive(i)}
-              className={`text-xl p-2.5 rounded-full sm:cursor-pointer bg-cylinder
-     ${i === active && "bg-dark_primary text-white"} `}
+              onClick={() => {
+                setActive(i);
+                setShowMenu(false);
+              }}
+              className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
+                i === active && "bg-dark_primary text-white"
+              }`}
             >
               {createElement(item.icon)}
             </a>
           ))}
         </nav>
-      )}
+      </div>
     </div>
   );
 };
