cbd48ac492433fc5372deae19b092845de310743
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-22 21:35:19 -0700

    third

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -8,15 +8,11 @@ const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [active, setActive] = useState(0);
 
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
   return (
     <div className="w-full flex justify-center relative">
       <div
         className="sm:cursor-pointer fixed top-10 left-10 z-[999] rounded-lg bg-white/40 p-2"
-        onClick={toggleMenu}
+        onClick={() => setShowMenu(!showMenu)}
       >
         <HiMenuAlt2 size={34} />
       </div>
@@ -29,9 +25,8 @@ const Navbar = () => {
               key={i}
               href={item.link}
               onClick={() => setActive(i)}
-              className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
-                i === active && "bg-dark_primary text-white"
-              }`}
+              className={`text-xl p-2.5 rounded-full sm:cursor-pointer
+     ${i === active && "bg-dark_primary text-white"} `}
             >
               {createElement(item.icon)}
             </a>
