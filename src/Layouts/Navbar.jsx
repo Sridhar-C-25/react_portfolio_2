6c405b76aa857d56eaf752d222d25df78727bdf1
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-22 21:57:59 -0700

    sixth

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -16,27 +16,25 @@ const Navbar = () => {
       >
         <HiMenuAlt2 size={34} />
       </div>
-      <nav
-        className={`fixed top-20 left-1/2 transform -translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-3xl text-dark_primary duration-300 ${
-          showMenu ? "bottom-0 opacity-100" : "bottom-[-80%] opacity-0"
-        }`}
-      >
-        {nav.map((item, i) => (
-          <a
-            key={i}
-            href={item.link}
-            onClick={() => setActive(i)}
-            className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
-              i === active && "bg-dark_primary text-white"
-            }`}
-          >
-            {createElement(item.icon)}
-          </a>
-        ))}
-      </nav>
+      {showMenu && (
+        <nav
+          className={`fixed top-20 left-10 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-md text-dark_primary duration-300`}
+        >
+          {nav.map((item, i) => (
+            <a
+              key={i} // Added key prop for mapping
+              href={item.link}
+              onClick={() => setActive(i)}
+              className={`text-xl p-2.5 rounded-full sm:cursor-pointer
+     ${i === active && "bg-dark_primary text-white"} `}
+            >
+              {createElement(item.icon)}
+            </a>
+          ))}
+        </nav>
+      )}
     </div>
   );
 };
 
 export default Navbar;
-
