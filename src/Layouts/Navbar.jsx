9bd53a04629e4f14f2cb3b8e99527961a282dccd
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-23 19:42:39 -0700

    4th

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -10,32 +10,36 @@ const Navbar = () => {
 
   return (
     <div className="w-full flex justify-left fixed pl-4 pt-4">
-      <div
-        className="sm:cursor-pointer z-[999] rounded-lg bg-white/40 p-2"
-        onClick={() => setShowMenu(!showMenu)}
-      >
-        <HiMenuAlt2 size={34} />
-        <nav
-          className={`absolute top-full right-1/2 transform translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-md text-dark_primary duration-300 ${
-            showMenu ? "opacity-100 translate-y-0" : "opacity-0 translate-y-3"
-          }`}
+      <div className="relative">
+        <div
+          className="sm:cursor-pointer z-[999] rounded-lg bg-white/40 p-2"
+          onClick={() => setShowMenu(!showMenu)}
         >
-          {nav.map((item, i) => (
-            <a
-              key={i}
-              href={item.link}
-              onClick={() => {
-                setActive(i);
-                setShowMenu(false);
-              }}
-              className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
-                i === active && "bg-dark_primary text-white"
-              }`}
-            >
-              {createElement(item.icon)}
-            </a>
-          ))}
-        </nav>
+          <HiMenuAlt2 size={34} />
+        </div>
+        {showMenu && (
+          <nav
+            className={`absolute top-full right-1/2 transform translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-md text-dark_primary duration-300 ${
+              showMenu ? "opacity-100 translate-y-0" : "opacity-0 translate-y-3"
+            }`}
+          >
+            {nav.map((item, i) => (
+              <a
+                key={i}
+                href={item.link}
+                onClick={() => {
+                  setActive(i);
+                  setShowMenu(false);
+                }}
+                className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
+                  i === active && "bg-dark_primary text-white"
+                }`}
+              >
+                {createElement(item.icon)}
+              </a>
+            ))}
+          </nav>
+        )}
       </div>
     </div>
   );
