814f86ce4cb3541168091b3bf12065bc4cefca33
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-24 19:37:57 -0700

    20th

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -7,36 +7,38 @@ const Navbar = () => {
   const { nav } = content;
   const [showMenu, setShowMenu] = useState(false);
   const [active, setActive] = useState(0);
+  const [expanded, setExpanded] = useState(false);
 
   const handleMenuClick = (index) => {
     setActive(index);
+    setExpanded(false);
   };
 
   const handleMenuToggle = () => {
     setShowMenu(!showMenu);
+    setExpanded(!expanded);
   };
 
   return (
     <div className="fixed top-4 left-4 z-[999]">
       <div className="relative">
         <div
-          className={`sm:cursor-pointer z-[999] rounded-lg p-2 bg-white/50`}
+          className={`sm:cursor-pointer z-[999] rounded-lg p-2 ${
+            expanded ? "bg-white/50 w-28" : "bg-transparent"
+          }`}
           onClick={handleMenuToggle}
           style={{ pointerEvents: showMenu ? "none" : "auto" }}
         >
           <HiMenuAlt2 size={34} />
         </div>
         {showMenu && (
-          <div
-            className="fixed inset-0"
-            onClick={handleMenuToggle}
-          />
+          <div className="fixed inset-0" onClick={handleMenuToggle} />
         )}
         <nav
           className={`${
             showMenu
-              ? "absolute top-16 left-1/2 -translate-x-1/2 z-[999] opacity-100"
-              : "opacity-0 pointer-events-none"
+              ? "absolute top-16 left-1/2 -translate-x-1/2 z-[999] opacity-100 w-28"
+              : "opacity-0 pointer-events-none w-0"
           } flex flex-col items-center gap-5 px-6 py-2 rounded-lg text-dark_primary`}
           style={{ background: "rgba(255, 255, 255, 0.5)", backdropFilter: "none" }}
         >
