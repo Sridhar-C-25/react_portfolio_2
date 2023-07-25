fcf9ee57ed14921ca392b3af717e8f05f9795cf6
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-24 19:39:39 -0700

    21th

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -7,40 +7,42 @@ const Navbar = () => {
   const { nav } = content;
   const [showMenu, setShowMenu] = useState(false);
   const [active, setActive] = useState(0);
-  const [expanded, setExpanded] = useState(false);
 
   const handleMenuClick = (index) => {
     setActive(index);
-    setExpanded(false);
   };
 
   const handleMenuToggle = () => {
     setShowMenu(!showMenu);
-    setExpanded(!expanded);
+  };
+
+  const menuStyle = {
+    width: showMenu ? "140px" : "40px", // Adjust the width of the menu background
   };
 
   return (
     <div className="fixed top-4 left-4 z-[999]">
       <div className="relative">
         <div
-          className={`sm:cursor-pointer z-[999] rounded-lg p-2 ${
-            expanded ? "bg-white/50 w-28" : "bg-transparent"
-          }`}
+          className={`sm:cursor-pointer z-[999] rounded-lg p-2 bg-white/50`}
           onClick={handleMenuToggle}
           style={{ pointerEvents: showMenu ? "none" : "auto" }}
         >
           <HiMenuAlt2 size={34} />
         </div>
         {showMenu && (
-          <div className="fixed inset-0" onClick={handleMenuToggle} />
+          <div
+            className="fixed inset-0"
+            onClick={handleMenuToggle}
+          />
         )}
         <nav
-          className={`${
-            showMenu
-              ? "absolute top-16 left-1/2 -translate-x-1/2 z-[999] opacity-100 w-28"
-              : "opacity-0 pointer-events-none w-0"
-          } flex flex-col items-center gap-5 px-6 py-2 rounded-lg text-dark_primary`}
-          style={{ background: "rgba(255, 255, 255, 0.5)", backdropFilter: "none" }}
+          className="absolute top-0 left-0 flex items-center gap-5 px-6 py-2 rounded-lg text-dark_primary"
+          style={{
+            ...menuStyle,
+            background: "rgba(255, 255, 255, 0.5)",
+            backdropFilter: "none",
+          }}
         >
           {nav.map((item, i) => (
             <a
