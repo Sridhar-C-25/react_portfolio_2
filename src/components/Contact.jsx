c2988738c82c015a1aa5d199a92804d878136bd8
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-27 20:19:01 -0700

    3rd

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,3 @@
-import { createElement } from "react";
 import { content } from "../Content";
 
 const Contact = () => {
@@ -20,32 +19,13 @@ const Contact = () => {
             className="flex-1 flex flex-col gap-5"
           >
             <h4 className="text-white">Email:</h4>
-            <a className="font-Poppins" href="mailto:mripekci@gmail.com">
-              mripekci@gmail.com
-            </a>
+            <p className="font-Poppins">{Contact.social_media[0].text}</p>
             <h4 className="text-white">Phone:</h4>
-            <a className="font-Poppins" href="tel:+14083388954">
-              +1 408-338-8954
-            </a>
+            <p className="font-Poppins">{Contact.social_media[1].text}</p>
             <h4 className="text-white">LinkedIn:</h4>
-            <a className="font-Poppins" href="https://www.linkedin.com/in/mustafa-ipekci-96a98925b/" target="_blank" rel="noopener noreferrer">
-              Mustafa Ipekci
+            <a className="font-Poppins" href={Contact.social_media[2].link} target="_blank" rel="noopener noreferrer">
+              {Contact.social_media[2].text}
             </a>
-          </div>
-          <div className="flex-1 flex flex-col gap-5">
-            {Contact.social_media.map((content, i) => (
-              <div
-                key={i}
-                data-aos="fade-down"
-                data-aos-delay={i * 430}
-                className="flex items-center gap-2"
-              >
-                <h4 className="text-white">{createElement(content.icon)}</h4>
-                <a className="font-Poppins" href={content.link} target="_blank" rel="noopener noreferrer">
-                  {content.text}
-                </a>
-              </div>
-            ))}
           </div>
         </div>
       </div>
