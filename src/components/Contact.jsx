c62243d95ca5cd83b81ebb409900a332712f8131
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-27 20:28:51 -0700

    7th

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,20 +12,12 @@ const Contact = () => {
         <h4 className="subtitle" data-aos="fade-down">
           {Contact.subtitle}
         </h4>
-        <div
-          className="flex gap-5"
-          data-aos="fade-up"
-          data-aos-delay="300"
-        >
-          <div className="flex items-center gap-2">
+        <div className="flex gap-5">
+          <div data-aos="fade-up" className="flex-1 flex flex-col gap-5">
             <h4 className="text-white">Email:</h4>
             <p className="font-Poppins">{Contact.social_media[0].text}</p>
-          </div>
-          <div className="flex items-center gap-2">
             <h4 className="text-white">Phone:</h4>
             <p className="font-Poppins">{Contact.social_media[1].text}</p>
-          </div>
-          <div className="flex items-center gap-2">
             <h4 className="text-white">LinkedIn:</h4>
             <a className="font-Poppins" href={Contact.social_media[2].link} target="_blank" rel="noopener noreferrer">
               {Contact.social_media[2].text}
