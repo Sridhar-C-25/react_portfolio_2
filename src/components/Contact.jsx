d65e47cc4fc283768052f6cadc15abb39b73602a
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-07-27 20:19:52 -0700

    5th

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { createElement } from "react";
 import { content } from "../Content";
 
 const Contact = () => {
@@ -18,14 +19,21 @@ const Contact = () => {
             data-aos="fade-up"
             className="flex-1 flex flex-col gap-5"
           >
-            <h4 className="text-white">Email:</h4>
-            <p className="font-Poppins">{Contact.social_media[0].text}</p>
-            <h4 className="text-white">Phone:</h4>
-            <p className="font-Poppins">{Contact.social_media[1].text}</p>
-            <h4 className="text-white">LinkedIn:</h4>
-            <a className="font-Poppins" href={Contact.social_media[2].link} target="_blank" rel="noopener noreferrer">
-              {Contact.social_media[2].text}
-            </a>
+          </div>
+          <div className="flex-1 flex flex-col gap-5">
+            {Contact.social_media.map((content, i) => (
+              <div
+                key={i}
+                data-aos="fade-down"
+                data-aos-delay={i * 430}
+                className="flex items-center gap-2"
+              >
+                <h4 className="text-white">{createElement(content.icon)}</h4>
+                <a className="font-Poppins" href={content.link} target="_blank" rel="noopener noreferrer">
+                  {content.text}
+                </a>
+              </div>
+            ))}
           </div>
         </div>
       </div>
