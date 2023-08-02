96ff78cbdcc235dce7f530a28210636937775856
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-08-01 21:27:13 -0700

    reset

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { content } from "../Content";
 import { HiMenuAlt2 } from "react-icons/hi";
 import { createElement } from "react";
-import styled, { keyframes } from "styled-components";
 
 const Navbar = () => {
   const { nav } = content;
@@ -10,68 +9,35 @@ const Navbar = () => {
   const [active, setActive] = useState(0);
 
   return (
-    <StyledNavbar>
+    <div className="fixed top-4 left-4 z-[999] flex flex-col items-center">
       <div className="relative">
-        {/* Button to toggle menu */}
         <div
           className={`sm:cursor-pointer z-[999] rounded-lg bg-white/40 p-2`}
           onClick={() => setShowMenu(!showMenu)}
         >
           <HiMenuAlt2 size={34} />
         </div>
-        {/* Menu contents */}
-        <StyledNav
-          showMenu={showMenu}
-          className={`flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-lg text-dark_primary duration-300`}
-        >
-          {nav.map((item, i) => (
-            <StyledNavItem
-              key={i}
-              href={item.link}
-              onClick={() => setActive(i)}
-              className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
-                i === active && "bg-dark_primary text-white"
-              } `}
-            >
-              {createElement(item.icon)}
-            </StyledNavItem>
-          ))}
-        </StyledNav>
+        {showMenu && (
+          <nav
+            className={`absolute top-full left-1/2 transform -translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300`}
+          >
+            {nav.map((item, i) => (
+              <a
+                key={i}
+                href={item.link}
+                onClick={() => setActive(i)}
+                className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
+                  i === active && "bg-dark_primary text-white"
+                } `}
+              >
+                {createElement(item.icon)}
+              </a>
+            ))}
+          </nav>
+        )}
       </div>
-    </StyledNavbar>
+    </div>
   );
 };
-
-const slideIn = keyframes`
-  from {
-    transform: translateX(-100%);
-  }
-  to {
-    transform: translateX(0);
-  }
-`;
-
-const slideOut = keyframes`
-  from {
-    transform: translateX(0);
-  }
-  to {
-    transform: translateX(-100%);
-  }
-`;
-
-const StyledNavbar = styled.div`
-  // Your existing styles for the Navbar container
-`;
-
-const StyledNav = styled.nav`
-  display: ${({ showMenu }) => (showMenu ? "flex" : "none")};
-  animation: ${({ showMenu }) => (showMenu ? slideIn : slideOut)} 0.3s ease forwards;
-  // Rest of your existing styles for the menu
-`;
-
-const StyledNavItem = styled.a`
-  // Your existing styles for the menu items
-`;
 
 export default Navbar;
