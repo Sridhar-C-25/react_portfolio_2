16386deaf393ae871422b3727d494fbdd0e49add
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-08-03 22:02:16 -0700

    6th

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -8,36 +8,37 @@ const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [active, setActive] = useState(0);
 
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="fixed top-4 left-4 z-[999] flex flex-col items-center">
       <div className="relative">
         <div
           className={`sm:cursor-pointer z-[999] rounded-lg bg-white/40 p-2`}
-          onClick={() => setShowMenu(!showMenu)}
+          onClick={toggleMenu}
         >
           <HiMenuAlt2 size={34} />
         </div>
-        <nav
-          className={`${
-            showMenu ? "open" : ""
-          } absolute top-full left-1/2 transform -translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300 transition-transform`}
-        >
-          {nav.map((item, i) => (
-            <a
-              key={i}
-              href={item.link}
-              onClick={() => {
-                setActive(i);
-                setShowMenu(false); // Close the menu when an item is clicked
-              }}
-              className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
-                i === active && "bg-dark_primary text-white"
-              } `}
-            >
-              {createElement(item.icon)}
-            </a>
-          ))}
-        </nav>
+        {showMenu && (
+          <nav
+            className={`absolute top-full left-1/2 transform -translate-x-1/2 z-[999] flex flex-col items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300`}
+          >
+            {nav.map((item, i) => (
+              <a
+                key={i}
+                href={item.link}
+                onClick={() => setActive(i)}
+                className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${
+                  i === active && "bg-dark_primary text-white"
+                } `}
+              >
+                {createElement(item.icon)}
+              </a>
+            ))}
+          </nav>
+        )}
       </div>
     </div>
   );
