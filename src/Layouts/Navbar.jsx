9175ef912897241086c3b1e6c5c6ccda810b086a
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-12 15:00:49 -0800

    new navbar

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
   const [active, setActive] = useState(0);
 
   return (
-    <div className="w-full flex justify-center">
+    <div className="w-full flex flex-col items-center">
       <div
         className="sm:cursor-pointer fixed top-10 left-10 z-[999] rounded-lg bg-white/40 p-2"
         onClick={() => setShowMenu(!showMenu)}
@@ -68,16 +68,16 @@ const Navbar = () => {
         <HiMenuAlt2 size={34} />
       </div>
       <nav
-        className={`fixed  z-[999] flex items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300 ${
+        className={`fixed z-[999] flex flex-col items-center bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300 ${
           showMenu ? "bottom-10" : "bottom-[-100%]"
         }`}
       >
         {nav.map((item, i) => (
           <a
+            key={i}
             href={item.link}
             onClick={() => setActive(i)}
-            className={`text-xl p-2.5 rounded-full sm:cursor-pointer 
-     ${i === active && "bg-dark_primary text-white"} `}
+            className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${i === active && "bg-dark_primary text-white"} `}
           >
             {createElement(item.icon)}
           </a>
