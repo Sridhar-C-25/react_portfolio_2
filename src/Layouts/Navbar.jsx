2998809ecdb1f9bad47f2300d829c9cd045fcd2c
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-12 15:44:44 -0800

    new push to fix the navigation

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -1,54 +1,3 @@
-/* import { useState } from "react";
-import { content } from "../Content";
-import { HiMenuAlt2 } from "react-icons/hi";
-import { createElement } from "react";
-
-const Navbar = () => {
-  const { nav } = content;
-  const [showMenu, setShowMenu] = useState(false);
-  const [active, setActive] = useState(0);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-  return (
-    <div className="fixed top-4 left-4 z-[999]">
-      <div className="relative">
-        <div
-          className={`sm:cursor-pointer z-[999] rounded-lg bg-white/40 p-2`}
-          onClick={toggleMenu}
-        >
-          <HiMenuAlt2 size={15} />
-        </div>
-        <nav
-          className={`${
-            showMenu ? "slide-in" : ""
-          } absolute top-14 left-0 z-[999] w-40 bg-slate-200/60 p-6 backdrop-blur-md rounded-lg text-dark_primary transition-transform duration-300`}
-        >
-          {nav.map((item, i) => (
-            <a
-              key={i}
-              href={item.link}
-              onClick={() => {
-                setActive(i);
-                setShowMenu(false);
-              }}
-              className={`text-xl p-2.5 rounded-full block mb-2 ${
-                i === active ? "bg-dark_primary text-white" : ""
-              } `}
-            >
-              {createElement(item.icon)}
-            </a>
-          ))}
-        </nav>
-      </div>
-    </div>
-  );
-};
-
-export default Navbar; */
-
 import { useState } from "react";
 import { content } from "../Content";
 import { HiMenuAlt2 } from "react-icons/hi";
@@ -71,6 +20,7 @@ const Navbar = () => {
         className={`fixed z-[999] flex flex-col items-start bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300 ${
           showMenu ? "top-16" : "-top-full"
         }`}
+        style={{ paddingTop: "10px", paddingLeft: "10px" }}
       >
         {nav.map((item, i) => (
           <a
