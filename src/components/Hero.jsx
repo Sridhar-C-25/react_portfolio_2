4c71b7cbbb35552b8b16772a5c88317469bbe302
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-12 15:54:57 -0800

    hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,4 @@
-// import content
+/*
 import React, { useRef } from "react";
 import { content } from "../Content";
 import { Application } from '@splinetool/runtime';
@@ -17,7 +17,6 @@ const Hero = () => {
     }
   };
 
-  // Call initializeSpline when the component is mounted
   initializeSpline();
 
   return (
@@ -28,7 +27,55 @@ const Hero = () => {
           data-aos-delay="1200"
           className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
+        </div>
+
+        {}
+        <div className="pb-16 px-6 pt-5" data-aos="fade-down">
+          <h2>{hero.title}</h2>
+          <br />
+          <div className="flex flex-col gap-10 mt-10">
+            {hero.hero_content.map((content, i) => (
+              <div
+                key={i}
+                data-aos="fade-down"
+                data-aos-delay={i * 300}
+                className={`flex items-center w-80 gap-5
+            ${i === 1 && " flex-row-reverse text-right"}  `}
+              >
+                <h3>{content.count}</h3>
+                <p>{content.text}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {}
+        <canvas ref={canvasRef} id="canvas3d"></canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Hero; */
+
+// import content
+import React from "react";
+import { content } from "../Content";
+
+const Hero = () => {
+  const { hero } = content;
+
+  return (
+    <section id="home" className="overflow-hidden">
+      <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
+        <div
+          data-aos="slide-left"
+          data-aos-delay="1200"
+          className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
+        >
           {/* Add anything */}
+          {/* Inject the spline-viewer script tag */}
+          <div dangerouslySetInnerHTML={{ __html: '<script type="module" src="https://unpkg.com/@splinetool/viewer@0.9.496/build/spline-viewer.js"></script><spline-viewer url="https://prod.spline.design/HKlfAoUoTv3w35yQ/scene.splinecode"></spline-viewer>' }} />
         </div>
 
         {/* first col */}
@@ -50,9 +97,6 @@ const Hero = () => {
             ))}
           </div>
         </div>
-
-        {/* Add a ref to the canvas element */}
-        <canvas ref={canvasRef} id="canvas3d"></canvas>
       </div>
     </section>
   );
