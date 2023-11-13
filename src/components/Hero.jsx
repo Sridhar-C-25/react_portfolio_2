0ff6620316765c695da314680e1afa43b0e186a0
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-12 16:19:44 -0800

    second try

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,42 +50,37 @@ export default Hero;*/
 
 import { useEffect } from "react";
 import { content } from "../Content";
-// Import the Spline styles if needed
-// import '@splinetool/react-spline/dist/index.css';
 
 const Hero = () => {
   const { hero } = content;
 
-  // Use useEffect to load the Spline scene when the component mounts
   useEffect(() => {
     const script = document.createElement("script");
+    script.type = "module";
     script.src = "https://unpkg.com/@splinetool/viewer@0.9.496/build/spline-viewer.js";
     script.async = true;
     script.onload = () => {
       const splineViewer = document.createElement("spline-viewer");
       splineViewer.setAttribute("url", "https://prod.spline.design/HKlfAoUoTv3w35yQ/scene.splinecode");
-      document.body.appendChild(splineViewer);
+      document.getElementById("hero-container").appendChild(splineViewer);
     };
     document.body.appendChild(script);
 
     return () => {
-      // Cleanup the script and viewer when the component unmounts
       document.body.removeChild(script);
       const splineViewer = document.querySelector("spline-viewer");
       if (splineViewer) {
-        document.body.removeChild(splineViewer);
+        document.getElementById("hero-container").removeChild(splineViewer);
       }
     };
   }, []);
 
   return (
     <section id="home" className="relative overflow-hidden">
-      <div className="min-h-screen flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
-        {/* The following div will act as the background */}
-        <div
-          className="absolute inset-0 w-full h-full bg-primaryLinear"
-          style={{ zIndex: -1 }}
-        />
+      <div
+        id="hero-container"
+        className="min-h-screen flex md:flex-row flex-col-reverse md:items-end justify-center items-center"
+      >
         <div
           data-aos="slide-left"
           data-aos-delay="1200"
@@ -122,3 +117,4 @@ const Hero = () => {
 };
 
 export default Hero;
+
