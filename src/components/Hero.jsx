f659606c569b5ef0ae04364ef50facccbc9aaf2f
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-16 15:37:29 -0800

    moving title and other such things

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ const Hero = () => {
   return (
     <section id="home" className="relative overflow-hidden">
       <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
+        <h2>{hero.title}</h2>
         <Spline scene="https://prod.spline.design/HKlfAoUoTv3w35yQ/scene.splinecode" className="absolute top-0 left-0 w-full h-full" />
 
         {/* Your text content */}
@@ -16,14 +17,14 @@ const Hero = () => {
           data-aos-delay="1200"
           className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
-          <h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
+          {/*<h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
             {hero.firstName}{" "}
             <span className="text-dark_primary">{hero.LastName}</span>
-          </h1>
+          </h1>*/}
         </div>
 
         <div className="pb-16 px-6 pt-5 z-10" data-aos="fade-down">
-          <h2>{hero.title}</h2>
+          
           <br />
           <div className="flex flex-col gap-10 mt-10">
             {hero.hero_content.map((content, i) => (
