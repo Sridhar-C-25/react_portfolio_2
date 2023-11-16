64e2e3cf72a6e144e9df230ef05670cc05f06745
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-16 15:42:45 -0800

    styling title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,15 +16,10 @@ const Hero = () => {
           data-aos-delay="1200"
           className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
-          <h2>{hero.title}</h2>
-          {/*<h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
-            {hero.firstName}{" "}
-            <span className="text-dark_primary">{hero.LastName}</span>
-          </h1>*/}
+          <h2 className="rotate-90 absolute top-[30%] right-[15%]">{hero.title}</h2>
         </div>
 
         <div className="pb-16 px-6 pt-5 z-10" data-aos="fade-down">
-          
           <br />
           <div className="flex flex-col gap-10 mt-10">
             {hero.hero_content.map((content, i) => (
