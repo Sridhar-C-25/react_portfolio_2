e6133d409028b4d08358e297a289ba0fa6f42db8
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-18 19:31:16 -0800

    slight adjustment to style

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
         className={`fixed z-[999] flex flex-col items-start bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300 ${
           showMenu ? "top-16" : "-top-full"
         }`}
-        style={{ paddingTop: '30px', paddingLeft: '30px' }}
+        style={{ paddingTop: '40px', paddingLeft: '40px' }}
       >
         {nav.map((item, i) => (
           <a
