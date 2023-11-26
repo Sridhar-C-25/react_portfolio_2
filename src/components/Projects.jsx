d19cfe8b044cd5a47ebcb4cfd849b4fa046c290c
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-11-25 18:14:38 -0800

    triplize the spine

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,15 @@
-import { useState } from "react";
-import { content } from "../Content";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/navigation";
-import { Navigation } from "swiper";
-import Spline from '@splinetool/react-spline'; // Make sure to import the Spline component
+import React, { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Navigation } from 'swiper';
+import Spline from '@splinetool/react-spline';
 
 // Import Swiper styles
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
 const Projects = () => {
-  const { Projects } = content;
   const [selectedProject, setSelectedProject] = useState(null);
 
   const openModal = (projectIndex) => {
@@ -22,25 +20,17 @@ const Projects = () => {
     setSelectedProject(null);
   };
 
-  const projectsWithSpline = [...Projects.project_content];
-  const splineProjectIndex = 1;
-
-  projectsWithSpline[splineProjectIndex] = {
-    title: "Mini Keyboard",
-    image: "src/assets/images/projects/lmao.png",
-    splineScene: "https://prod.spline.design/VaWzQnJylRSKhxe8/scene.splinecode",
-    description: "This is a keyboard model made in the Spline 3D model workshop that is integrated fully into this react website.",
-  };
+  const projectsCount = 3; // Change this to the number of slides you want
 
   return (
     <section className="bg-bg_light_primary" id="projects">
       <div className="md:container px-5 pt-14 h-full flex flex-col justify-center items-center relative">
         <div>
           <h2 className="title" data-aos="fade-down">
-            {Projects.title}
+            Project Title
           </h2>
           <h4 className="subtitle" data-aos="fade-down">
-            {Projects.subtitle}
+            Project Subtitle
           </h4>
           <br />
         </div>
@@ -55,14 +45,18 @@ const Projects = () => {
             modules={[Navigation]}
             className="rounded-3xl pb-16 max-w-[60vw] w-full drop-shadow-primary self-start relative"
           >
-            {projectsWithSpline.map((content, i) => (
+            {[...Array(projectsCount)].map((_, i) => (
               <SwiperSlide
                 key={i}
                 className="bg-white rounded-3xl p-5 border-b-8 border-[#FAF9FD] h-fit relative"
               >
-                <img src={content.image} alt="..." className="w-full h-[200px] object-cover mb-4" />
+                <img
+                  src="src/assets/images/projects/lmao.png"
+                  alt="..."
+                  className="w-full h-[200px] object-cover mb-4"
+                />
                 <div className="flex flex-col gap-1 mt-2">
-                  <h5 className="font-bold font-Poppins">{content.title}</h5>
+                  <h5 className="font-bold font-Poppins">Mini Keyboard</h5>
                   <button
                     className="font-bold text-gray self-end"
                     onClick={() => openModal(i)}
@@ -81,54 +75,61 @@ const Projects = () => {
       {selectedProject !== null && (
         <div
           style={{
-            position: "fixed",
+            position: 'fixed',
             top: 0,
             left: 0,
-            width: "100%",
-            height: "100%",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'rgba(0, 0, 0, 0.7)',
             zIndex: 1000,
           }}
           onClick={closeModal}
         >
           <div
             style={{
-              backgroundColor: "white",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              position: "relative",
-              width: "80%",
-              height: "80%",
-              padding: "20px",
+              backgroundColor: 'white',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              position: 'relative',
+              width: '80%',
+              height: '80%',
+              padding: '20px',
             }}
             onClick={(e) => e.stopPropagation()}
           >
-            <h2>{projectsWithSpline[selectedProject].title}</h2>
-            <p>{/* Add content specific to the selected project */}</p>
-            {projectsWithSpline[selectedProject].splineScene && (
-              <div style={{ width: "100%", height: "100%" }}>
-                <Spline scene={projectsWithSpline[selectedProject].splineScene} className="w-full h-full" />
-              </div>
-            )}
+            <h2>Mini Keyboard</h2>
+            <p>
+              This is a keyboard model made in the Spline 3D model workshop that is integrated fully
+              into this react website.
+            </p>
+            <div style={{ width: '100%', height: '100%' }}>
+              <Spline
+                scene="https://prod.spline.design/VaWzQnJylRSKhxe8/scene.splinecode"
+                className="w-full h-full"
+              />
+            </div>
             <div className="absolute bottom-0 right-0 p-4">
-              <p>{projectsWithSpline[selectedProject].description}</p>
+              <p>
+                This is a keyboard model made in the Spline 3D model workshop that is integrated
+                fully into this react website.
+              </p>
             </div>
             <button
               onClick={closeModal}
               style={{
-                position: "absolute",
-                top: "20px",
-                right: "20px",
-                backgroundColor: "transparent",
-                border: "none",
-                cursor: "pointer",
+                position: 'absolute',
+                top: '20px',
+                right: '20px',
+                backgroundColor: 'transparent',
+                border: 'none',
+                cursor: 'pointer',
               }}
             >
-              <span style={{ fontSize: "24px", fontWeight: "bold" }}>X</span>
+              <span style={{ fontSize: '24px', fontWeight: 'bold' }}>X</span>
             </button>
           </div>
         </div>
