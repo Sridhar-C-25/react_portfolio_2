3996051b9807d6660f791916efda3b575b5bbe6a
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-23 08:58:32 -0800

    spline rerfix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,7 +80,7 @@ export default Hero;
 
 import React, { useState, useEffect } from "react";
 import { content } from "../Content";
-//import { Spline } from "@splinetool/react-spline";
+import { Spline } from "@splinetool/react-spline";
 
 const ReactSpline = import("@splinetool/react-spline");
 
