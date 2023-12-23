6a62d9b4a3a1bc658b0d86a7c61eea7b881ece9c
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-22 19:55:35 -0800

    boonga

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@splinetool/react-spline'],
+  resolve: {
+    alias: {
+      '@splinetool/react-spline': '@splinetool/react-spline',
     },
   },
 });
+
 
 
 
