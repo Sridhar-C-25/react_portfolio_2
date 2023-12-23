bdd26ad9120f160b92de236c7b1c170daa54ab35
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-22 18:39:05 -0800

    final hopefully

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { content } from "../Content";
-import ReactSpline from "@splinetool/react-spline";
-import ReactSpline from "../../node_modules/@splinetool/react-spline";
+import ReactSpline from "../../src/@splinetool/react-spline";
 
 const Hero = () => {
   const { hero } = content;
