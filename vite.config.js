c7b64caf9826ea72dd7dd57f6c974d7794fe2107
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-22 19:46:12 -0800

    idk

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [], // Remove any entries from this array
+    },
+  },
 });
+
 
 
 
