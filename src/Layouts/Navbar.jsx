89ec2b349c711f8bff517b69108e605af3256f00
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-22 17:00:17 -0800

    nav 2

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { content } from "../Content";
 import { HiMenuAlt2 } from "react-icons/hi";
-import { FiMoon, FiSun } from "react-icons/fi"; // Assuming FiMoon and FiSun are dark and light mode icons
+import { FiMoon, FiSun } from "react-icons/fi";
 
 const Navbar = () => {
   const { nav } = content;
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <div className="w-full flex flex-col items-start">
       <div
-        className="sm:cursor-pointer fixed top-10 left-10 z-[999] rounded-lg bg-primaryLinear p-2"
+        className="cursor-pointer fixed top-10 left-10 z-[999] rounded-lg bg-primaryLinear p-2"
         onClick={() => setShowMenu(!showMenu)}
       >
         <HiMenuAlt2 size={34} />
@@ -35,14 +35,14 @@ const Navbar = () => {
             key={i}
             href={item.link}
             onClick={() => setActive(i)}
-            className={`text-xl p-2.5 rounded-full sm:cursor-pointer ${i === active && "bg-dark_primary text-white"} `}
+            className={`text-xl p-2.5 rounded-full cursor-pointer ${i === active ? "bg-dark_primary text-white" : ""} `}
           >
             {createElement(item.icon)}
           </a>
         ))}
         <button
           onClick={toggleDarkMode}
-          className="text-xl p-2.5 rounded-full sm:cursor-pointer"
+          className="text-xl p-2.5 rounded-full cursor-pointer"
         >
           {darkMode ? <FiSun /> : <FiMoon />}
         </button>
