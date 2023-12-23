39ca9ca2732b0512731e6f08332dcf7bc38545b2
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-22 18:51:07 -0800

    4th

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,13 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@splinetool/react-spline': './path/to/your/src/@splinetool/react-spline',
+      '@splinetool/react-spline': './@splinetool/react-spline',
     },
-  },
-  optimizeDeps: {
-    include: ['src/components/Hero.jsx'],
   },
 });
