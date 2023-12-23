160c9952c2424458901298edf0fe4a7c26b7016c
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-22 18:54:34 -0800

    7th

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,26 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@splinetool/react-spline': './@splinetool/react-spline/index.js',
+      '@splinetool/react-spline': './src/@splinetool/react-spline/index.js',
     },
   },
 });
+
+
+
+
+
+/*
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: /^@splinetool\/react-spline/,
+    },
+  },
+});
+*/
