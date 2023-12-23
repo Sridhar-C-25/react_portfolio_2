6513735a8bb535ea87265b538026ab5fe4aa211b
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-22 16:16:35 -0800

    fixing blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,30 +2,29 @@ import React from "react";
 import { content } from "../Content";
 
 const Blog = () => {
-    const { blog } = content;
+  const { blog } = content;
 
-    return (
-      <section id="home" className="relative overflow-hidden">
-        <style>
-          {`
-            @media only screen and (max-width: 767px) {
-              .flex-col-reverse-mobile {
-                flex-direction: column-reverse !important;
-              }
+  return (
+    <section id="home" className="relative overflow-hidden">
+      <style>
+        {`
+          @media only screen and (max-width: 767px) {
+            .flex-col-reverse-mobile {
+              flex-direction: column-reverse !important;
+            }
 
-              .text-center-mobile {
-                text-align: center !important;
-              }
+            .text-center-mobile {
+              text-align: center !important;
             }
           `}
-        </style>
-        <div>
-            <h1>
-                Hello this is blog!
-            </h1>
-        </div>
-      </section>
-    );
-},
+      </style>
+      <div>
+        <h1>
+          Hello, this is blog!
+        </h1>
+      </div>
+    </section>
+  );
+};
 
 export default Blog;
