92cdf9be08f07df894bcf462d4e232cbc881214e
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-23 11:41:49 -0800

    hero temporary spline removal

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+/*
+
 import React, { useState, useEffect, useRef } from "react";
 import { content } from "../Content";
 
@@ -41,7 +43,6 @@ const Hero = () => {
       </style>
 
       <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
-        {/* Render Spline component when it's loaded */}
         {SplineComponent && (
           <SplineComponent
             scene="https://prod.spline.design/HKlfAoUoTv3w35yQ/scene.splinecode"
@@ -71,6 +72,84 @@ const Hero = () => {
                 <p>
                   <h3>{content.count}</h3> {content.text}
                 </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
+
+*/
+
+
+// Hero.jsx
+
+import React, { useState, useEffect } from "react";
+import { content } from "../Content";
+
+const Hero = () => {
+  const { hero } = content;
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const threshold = window.innerHeight / 2;
+      const isScrolled = window.scrollY > threshold;
+      setShowContent(isScrolled);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <section id="home" className="relative overflow-hidden">
+      <style>
+        {`
+          @media only screen and (max-width: 767px) {
+            .flex-col-reverse-mobile {
+              flex-direction: column-reverse !important;
+            }
+
+            .text-center-mobile {
+              text-align: center !important;
+            }
+          }
+        `}
+      </style>
+
+      <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
+        <div
+          data-aos="slide-left"
+          data-aos-delay="1200"
+          className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
+        >
+          <h2 className="rotate-90 absolute top-[30%] right-[15%]">{hero.title}</h2>
+        </div>
+
+        <div className="pb-16 px-6 pt-12 z-10 text-center-mobile" data-aos="fade-down">
+          <br />
+          <div className="flex flex-col gap-10 mt-10">
+            {hero.hero_content.map((content, i) => (
+              <div
+                key={i}
+                data-aos="fade-down"
+                data-aos-delay={i * 300}
+                className={`flex items-center w-80
+                  ${i === 1 && " flex-row-reverse text-right flex-col-reverse-mobile"}
+                `}
+              >
+                {/* Use div or other suitable container instead of p for the parent element */}
+                <div>
+                  <h3>{content.count}</h3>
+                  <p>{content.text}</p>
+                </div>
               </div>
             ))}
           </div>
