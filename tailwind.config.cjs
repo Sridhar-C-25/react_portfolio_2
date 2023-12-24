519228e71aff16c3ed8282ef7de3c3a66235f0b9
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-23 17:56:39 -0800

    vite and tailwind changes to temp remove spline

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      Poppins: "Poppins",
-      Paprika: "Paprika",
-      Inria: "Inria Serif",
-    },
-    container: {
-      center: true,
-      padding: "4rem",
-    },
     extend: {
       colors: {
         dark_primary: "#06223F",
