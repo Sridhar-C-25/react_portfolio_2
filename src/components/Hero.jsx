d2a594d9907ba24d5248e0baf00dab88117eba50
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-24 16:58:43 -0800

    hero 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ const Hero = () => {
           data-aos-delay="1200"
           className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
         >
-          <h2 className="rotate-90 absolute top-[30%] right-[15%]" style={{ fontSize: '2rem' }}>{hero.title}</h2>
+          <h2 className="rotate-90 absolute top-[30%] right-[15%]" style={{ fontSize: '6rem' }}>{hero.title}</h2>
         </div>
 
         <div className="pb-16 px-6 pt-12 z-10 text-center-mobile" data-aos="fade-down">
