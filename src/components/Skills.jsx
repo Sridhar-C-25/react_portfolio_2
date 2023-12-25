ead1ee2ced667d368023285ae22e5252fdfd101d
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-24 18:33:13 -0800

    3rd

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,28 +1,44 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { content } from '../Content';
-import { useInView } from 'react-intersection-observer';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Skills = () => {
   const { skills } = content;
-  const [ref, inView] = useInView({ triggerOnce: true });
+  const skillsRef = useRef(null);
+
+  useEffect(() => {
+    AOS.init({ once: true });
+  }, []);
+
+  useEffect(() => {
+    if (skillsRef.current) {
+      AOS.refresh();
+    }
+  }, [skills]);
 
   return (
     <section
-      ref={ref}
-      className={`min-h-fit bg-bg_light_primary ${inView ? 'fade-in' : 'invisible'}`}
+      ref={skillsRef}
+      className="min-h-fit bg-bg_light_primary"
       id="skills"
+      data-aos="fade-up"
     >
       <div className="md:container px-5 py-14">
-        <h2 className="title">{skills.title}</h2>
-        <h4 className="subtitle">{skills.subtitle}</h4>
+        <h2 className="title" data-aos="fade-down">
+          {skills.title}
+        </h2>
+        <h4 className="subtitle" data-aos="fade-down">
+          {skills.subtitle}
+        </h4>
         <br />
         <div className="flex flex-wrap gap-4 justify-center">
           {skills.skills_content.map((skill, i) => (
             <div
               key={i}
-              className={`bg-white sm:cursor-default relative group w-full flex items-center gap-5 p-5 max-w-sm rounded-md border-2 border-slate-200 ${
-                inView ? 'fade-in' : 'invisible'
-              }`}
+              data-aos="fade-up"
+              data-aos-delay={i * 400}
+              className="bg-white sm:cursor-default relative group w-full flex items-center gap-5 p-5 max-w-sm rounded-md border-2 border-slate-200"
             >
               <div>
                 <img
@@ -44,3 +60,4 @@ const Skills = () => {
 };
 
 export default Skills;
+
