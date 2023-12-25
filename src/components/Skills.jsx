28b1bc1df153d5c5ab719f4dd02137cfb1352835
M-Ipekci <107224781+M-Ipekci@users.noreply.github.com>
2023-12-24 18:21:25 -0800

    new style

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { content } from '../Content';
 
 const Skills = () => {
   const { skills } = content;
+  const [isVisible, setIsVisible] = useState(false);
+  const skillsSectionRef = useRef(null);
+
+  const handleIntersection = (entries, observer) => {
+    const [entry] = entries;
+    if (entry.isIntersecting) {
+      setIsVisible(true);
+      observer.unobserve(entry.target);
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleIntersection, {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.3, // Adjust this threshold as needed
+    });
+
+    if (skillsSectionRef.current) {
+      observer.observe(skillsSectionRef.current);
+    }
+
+    return () => {
+      if (skillsSectionRef.current) {
+        observer.unobserve(skillsSectionRef.current);
+      }
+    };
+  }, []);
 
   return (
-    <section className="min-h-fit bg-bg_light_primary" id="skills" data-aos="fade-up">
+    <section
+      ref={skillsSectionRef}
+      className={`min-h-fit bg-bg_light_primary ${
+        isVisible ? 'fade-in' : 'invisible'
+      }`}
+      id="skills"
+    >
       <div className="md:container px-5 py-14">
-        <h2 className="title" data-aos="fade-down">
-          {skills.title}
-        </h2>
-        <h4 className="subtitle" data-aos="fade-down">
-          {skills.subtitle}
-        </h4>
+        <h2 className="title">{skills.title}</h2>
+        <h4 className="subtitle">{skills.subtitle}</h4>
         <br />
         <div className="flex flex-wrap gap-4 justify-center">
           {skills.skills_content.map((skill, i) => (
             <div
               key={i}
-              data-aos="fade-up"
-              data-aos-delay={i * 400}
-              data-aos-once="true" // Add this line to make the animation occur only once
-              className="bg-white sm:cursor-default
-               relative group w-full flex items-center
-                gap-5 p-5 max-w-sm rounded-md border-2 border-slate-200"
+              className={`bg-white sm:cursor-default relative group w-full flex items-center gap-5 p-5 max-w-sm rounded-md border-2 border-slate-200 ${
+                isVisible ? 'fade-in' : 'invisible'
+              }`}
             >
               <div>
                 <img
